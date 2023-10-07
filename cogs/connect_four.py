b9280bdea6245ba82df3f5ba6807d7f29989a7c9
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-10-07 16:32:25 +0800

    Connect Four AI v0.5

--- cogs/connect_four.py
+++ cogs/connect_four.py
@@ -1,5 +1,7 @@
 # Connect Four v1.0
 
+import math
+from copy import deepcopy
 import random
 import discord
 from discord.ext import commands
@@ -7,6 +9,13 @@ import numpy as np
 from scipy.signal import convolve2d
 from assets.enums import C4PlaceResults
 from assets.enums import C4GameResults
+
+
+# This class replaces a Discord member with the .mention and .display_name attributes
+class DummyAI:
+    def __init__(self, level):
+        self.mention = f"AI (Level {level})"
+        self.display_name = f"AI (Level {level})"
 
 
 # Each game is represented by a class instance to allow multiple games at once.
@@ -34,7 +43,7 @@ class C4Game:
         input_col = input_col - 1
 
         # Simulates the gravity needed when usually playing Connect Four.
-        # Each column's row is checked for bottom to top for an empty square,
+        # Each column's row is checked from bottom to top for an empty square,
         # and the piece is placed in that square
         for i in range(self.ROWS):
             if self.game_board[i][input_col] == 0:
@@ -45,7 +54,12 @@ class C4Game:
             # No available space left for the piece
             return C4PlaceResults.NO_SPACE
 
-    async def check_result(self):
+    async def check_result(self, board=None, turn=None):
+        if board is None:
+            board = self.game_board
+        if turn is None:
+            turn = self.turn
+
         # idk how this works but it's super cool!
         horizontal_kernel = np.array([[1, 1, 1, 1]])
         vertical_kernel = np.transpose(horizontal_kernel)
@@ -54,7 +68,7 @@ class C4Game:
         detection_kernels = [horizontal_kernel, vertical_kernel, diag1_kernel, diag2_kernel]
 
         for kernel in detection_kernels:
-            if (convolve2d(np.array(self.game_board) == self.turn + 1, kernel, mode="valid") == 4).any():
+            if (convolve2d(np.array(board) == turn + 1, kernel, mode="valid") == 4).any():
                 return C4GameResults.WIN
 
         # all(top_row) can be done here as the game is a draw when
@@ -139,6 +153,228 @@ class C4Game:
         await ctx.send(self.players[self.turn].mention, embed=embed)
 
 
+async def evaluate_window(window, piece):
+    score = 0
+    opp_piece = 1
+    if piece == 1:
+        opp_piece = 2
+
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score += 2
+
+    if window.count(opp_piece) == 3 and window.count(0) == 1:
+        score -= 4
+
+    return score
+
+
+class C4GameAI(C4Game):
+    def __init__(self, p1, p2, level):
+        super().__init__(p1, p2)
+        self.level = level
+        self.game_board = np.zeros((self.ROWS, self.COLUMNS)).tolist()
+
+    def is_valid_location(self, board, col):
+        return board[self.ROWS - 1][col] == 0
+
+    def get_next_open_row(self, board, col):
+        for r in range(self.ROWS):
+            if board[r][col] == 0:
+                return r
+
+    async def place(self, input_col):
+        input_col = input_col - 1
+        for i in range(self.ROWS):
+            if self.game_board[i][input_col] == 0:
+                self.game_board[i][input_col] = self.turn + 1
+                return
+
+    async def in_place(self, board, input_col, piece):
+        for i in range(self.ROWS):
+           if board[i][input_col] == 0:
+                board[i][input_col] = piece
+                return
+
+    async def possible_columns(self, board):
+        cols = []
+
+        for i in range(self.COLUMNS):
+            if board[-1][i] == 0:
+                cols.append(i)
+
+        return cols
+
+    async def is_terminal(self, board):
+        result = await self.check_result(turn=0, board=board)
+        result2 = await self.check_result(turn=1, board=board)
+        return result == C4GameResults.WIN or result2 == C4GameResults.WIN or result == C4GameResults.DRAW
+
+    async def winning_move(self, board, piece):
+        # Check horizontal locations for win
+        for c in range(self.COLUMNS - 3):
+            for r in range(self.ROWS):
+                if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
+                        c + 3] == piece:
+                    return True
+
+        # Check vertical locations for win
+        for c in range(self.COLUMNS):
+            for r in range(self.ROWS - 3):
+                if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
+                        c] == piece:
+                    return True
+
+        # Check positively sloped diagonals
+        for c in range(self.COLUMNS - 3):
+            for r in range(self.ROWS - 3):
+                if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and \
+                        board[r + 3][c + 3] == piece:
+                    return True
+
+        # Check negatively sloped diagonals
+        for c in range(self.COLUMNS - 3):
+            for r in range(3, self.ROWS):
+                if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
+                        board[r - 3][c + 3] == piece:
+                    return True
+
+    async def heuristic(self, board, piece):
+        # Keith Galli's scoring function
+
+        score = 0
+
+        # Score center column
+        center_array = [int(i) for i in list(board[:, self.COLUMNS // 2])]
+        center_count = center_array.count(piece)
+        score += center_count * 3
+
+        # Score Horizontal
+        for r in range(self.ROWS):
+            row_array = [int(i) for i in list(board[r, :])]
+            for c in range(self.COLUMNS - 3):
+                window = row_array[c:c + 4]
+                score += await evaluate_window(window, piece)
+
+        # Score Vertical
+        for c in range(self.COLUMNS):
+            col_array = [int(i) for i in list(board[:, c])]
+            for r in range(self.ROWS - 3):
+                window = col_array[r:r + 4]
+                score += await evaluate_window(window, piece)
+
+        # Score positive sloped diagonal
+        for r in range(self.ROWS - 3):
+            for c in range(self.COLUMNS - 3):
+                window = [board[r + i][c + i] for i in range(4)]
+                score += await evaluate_window(window, piece)
+
+        for r in range(self.ROWS - 3):
+            for c in range(self.COLUMNS - 3):
+                window = [board[r + 3 - i][c + i] for i in range(4)]
+                score += await evaluate_window(window, piece)
+
+        return score
+
+    async def minimax(self, board, depth, alpha, beta, max_player):
+        # Minimax algorithm
+        # This algorithm recursively generates the game tree
+        # and chooses the best path based on a scoring function.
+
+        valid_cols = await self.possible_columns(board)
+        is_terminal = await self.is_terminal(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if await self.winning_move(board, 2):
+                    return None, math.inf
+                elif await self.winning_move(board, 1):
+                    return None, -math.inf
+                else:
+                    return None, await self.heuristic(board, 2)
+        if max_player:
+            value = -math.inf
+            column = random.choice(valid_cols)
+            for col in valid_cols:
+                b_copy = board.copy()
+                await self.in_place(b_copy, col, 2)
+                new_score = (await self.minimax(b_copy, depth - 1, alpha, beta, False))[1]
+
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+
+            return column, value
+        else:
+            value = math.inf
+            column = random.choice(valid_cols)
+            for col in valid_cols:
+                b_copy = board.copy()
+                await self.in_place(b_copy, col, 1)
+                new_score = (await self.minimax(b_copy, depth - 1, alpha, beta, True))[1]
+
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+
+            return column, value
+
+    async def game_over(self, bot, ctx, result):
+        embed = discord.Embed(
+            title=f"{self.players[0].display_name} and {self.players[1].display_name}'s game:",
+            color=self.embed_color
+        )
+
+        board = ""
+        # Representing the game board in an embed
+        # The game board is reversed to display it top to bottom instead of bottom to top
+        for row in reversed(self.game_board):
+            for col in row:
+                if col == 0:
+                    board += self.empty
+                elif col == 1:
+                    board += self.p1piece
+                elif col == 2:
+                    board += self.p2piece
+            # Newline for next row in the board
+            board += "\n"
+
+        coin_amount = random.randint(100, 300)
+        if result == C4GameResults.WIN:
+            # Only add coins if the player won
+            if self.turn == 0:
+                await bot.database.add_member_coins(self.players[self.turn], coin_amount)
+
+                embed.description = (f"**{self.players[self.turn].mention} has won! {coin_amount}** "
+                                     f":coin: has been added to their balance.")
+            else:
+                embed.description = f"**{self.players[self.turn].mention} has won!**"
+        elif result == C4GameResults.DRAW:
+            embed.description = f"The game has ended in a draw."
+        elif result == C4GameResults.FORFEIT:
+            # 1 - self.turn gives the opposite of the turn
+            embed.description = (f"**{self.players[1 - self.turn].mention} has won** because of "
+                                 f"{self.players[self.turn].mention}'s forfeit. **{coin_amount}** "
+                                 f":coin: has been added to their balance.")
+
+        embed.add_field(name="Game", value=board)
+        await ctx.send(self.players[self.turn].mention, embed=embed)
+
+    async def ai_place(self):
+        b_copy = deepcopy(self.game_board)
+        col, _ = await self.minimax(b_copy, self.level, -math.inf, math.inf, True)
+
+        await self.place(col + 1)
+
+
 class ConnectFour(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -188,6 +424,52 @@ class ConnectFour(commands.Cog):
             await ctx.send("Please send j.c4 \"@Member\", where @Member is the member you want to play against.")
         else:
             print(error)
+
+    @commands.command(aliases=[
+        "c4ai", "connect_4ai", "connectfourai", "connect4ai",
+        "c4_ai", "connect_4_ai", "connectfour_ai", "connect4_ai", "connect_fourai"
+    ])
+    async def connect_four_ai(self, ctx, level: int):
+        game = C4GameAI(ctx.author, DummyAI(level), level)
+        color_guide = True
+
+        while True:
+            await game.ctx_send_board(ctx, color_guide=color_guide)
+            color_guide = False
+
+            while True:
+                def check(m):
+                    # Message author is the player with the turn, and the channel is right
+                    return m.author == ctx.author and m.channel == ctx.channel
+
+                player_input = await self.bot.wait_for('message', check=check)
+                player_input = player_input.content
+                if player_input.isnumeric():
+                    # Input is a number, proceed if number is between 1 and 7
+                    if 1 <= int(player_input) <= 7:
+                        await game.place(int(player_input))
+                        break
+                elif player_input.lower() in ("forfeit", "ff", "resign", ":handshake:", "�"):
+                    await game.game_over(self.bot, ctx, C4GameResults.FORFEIT)
+                    return
+                else:
+                    continue
+
+            result = await game.check_result()
+            if result in (C4GameResults.WIN, C4GameResults.DRAW):
+                await game.game_over(self.bot, ctx, result)
+                return
+
+            await game.swap_players()
+
+            await game.ai_place()
+
+            result = await game.check_result()
+            if result in (C4GameResults.WIN, C4GameResults.DRAW):
+                await game.game_over(self.bot, ctx, result)
+                return
+
+            await game.swap_players()
 
 
 async def setup(bot):
